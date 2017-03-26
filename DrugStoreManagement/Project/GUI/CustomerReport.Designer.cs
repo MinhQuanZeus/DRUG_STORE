85d6aa0ac280c659a85c1c6f0efe062c3fc81f13
DESKTOP-S935OJ9\Chido <linhchivuive@gmail.com>
2017-03-26 09:23:37 +0700

    Customer Report

--- DrugStoreManagement/Project/GUI/CustomerReport.Designer.cs
+++ DrugStoreManagement/Project/GUI/CustomerReport.Designer.cs
@@ -28,11 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Location = new System.Drawing.Point(16, 83);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(100, 123);
+            this.panel1.TabIndex = 0;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(9, 28);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(58, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Benefit";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(9, 86);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(67, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Products";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(122, 83);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(750, 150);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // CustomerReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(886, 309);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "CustomerReport";
             this.Text = "CustomerReport";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- DrugStoreManagement/Project/GUI/CustomerReport.cs
+++ DrugStoreManagement/Project/GUI/CustomerReport.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project.DAL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,20 @@ namespace Project.GUI
         public CustomerReport()
         {
             InitializeComponent();
+            DataTable dt = DAO.GetDataTable("select Customers.CustomerID,Customers.Name,Customers.Address,sum(Bills.BillID) as TotalBill,sum(BillDetails.SellPrice) as Buy,sum(BillDetails.SellPrice)-sum(BillDetails.Price) as Benefit , Bills.isDebt from Customers,Bills,BillDetails  where Customers.CustomerID = Bills.CustomerID and Bills.BillID = BillDetails.BillID group by Customers.CustomerID, Customers.Name, Customers.Address, Bills.isDebt");
+            dataGridView1.DataSource = dt;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            DataTable dt1 = DAO.GetDataTable("select Customers.CustomerID,Customers.Name,Customers.Address,sum(Bills.BillID) as TotalBill,sum(BillDetails.SellPrice) as Buy,sum(BillDetails.SellPrice)-sum(BillDetails.Price) as Benefit , Bills.isDebt from Customers,Bills,BillDetails  where Customers.CustomerID = Bills.CustomerID and Bills.BillID = BillDetails.BillID group by Customers.CustomerID, Customers.Name, Customers.Address, Bills.isDebt");
+            dataGridView1.DataSource = dt1;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            DataTable dt2 = DAO.GetDataTable("select Customers.CustomerID,Customers.Name,Customers.Address,Products.Name as ProductName, sum(BillDetails.Quantity) as Quantity from Customers,Bills,BillDetails,Products where Customers.CustomerID = Bills.CustomerID and Bills.BillID = BillDetails.BillID and BillDetails.ProductID = Products.ProductID group by Customers.CustomerID, Customers.Name, Products.Name, Customers.Address order by Customers.CustomerID");
+            dataGridView1.DataSource = dt2;
         }
     }
 }
